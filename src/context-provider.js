a4ace2e5532779a12806aeb74bc68323389a47f3
Yannis Herbig <yannis.herbig@hs-bochum.de>
2020-05-16 00:54:51 +0200

    added ngsi-parser to help extending the json-object into ngsi-format and using it to add an ID as well as a type; transforming the project into a module to enable the use of es6 syntax

--- src/context-provider.js
+++ src/context-provider.js
@@ -1,10 +1,34 @@
-const fetch = require('node-fetch');
+import NGSI from 'ngsi-parser';
+import fetch from 'node-fetch';
+
+const pvRequestURL = 'https://api.scit-bo.de/pv?apiKey=af7d173c57f5c4816747ada9e7aad23e';
+const weatherRequestURL = 'https://api.scit-bo.de/weather?apiKey=af7d173c57f5c4816747ada9e7aad23e';
 
 function requestData() { 
-	fetch('https://api.scit-bo.de/pv?apiKey=af7d173c57f5c4816747ada9e7aad23e', {
-	method: 'get'
+
+	fetch(pvRequestURL, {
+		method: 'get'
 	})
 	.then(res => res.json())
-    .then(json => console.log(json));
+	.then(json => {
+			json.id = Math.random();
+			json.type = "PV";
+			var entity = NGSI.parseEntity(json);
+			console.log(entity);
+		}
+	);
+	
+	fetch(weatherRequestURL, {
+		method: 'get'
+	})
+	.then(res => res.json())
+	.then(json => {
+			json.id = Math.random();
+			json.type = "WeatherObserved";
+			var entity = NGSI.parseEntity(json);
+			console.log(entity);
+		}
+	);
 } 
-requestData()
+
+requestData();
