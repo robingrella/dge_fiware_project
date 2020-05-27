5703cc8480b1d1d30aa1310d4b16269e431bfe66
Yannis Herbig <yannis.herbig@hs-bochum.de>
2020-05-28 00:50:05 +0200

    Entity wird jetzt zum Broker gepostet, aber ID-Vergabe müsste noch falsch sein

--- src/context-provider.js
+++ src/context-provider.js
@@ -5,6 +5,7 @@ import config from './config.js';
 
 const pvRequestURL = config.get('pv_request_url') + '?apiKey=' + config.get('api_key');
 const weatherRequestURL = config.get('weather_request_url') + '?apiKey=' + config.get('api_key');
+const contextBrokerEntitiesURL = config.get('context_broker_url') + "entities";
 
 function requestPV() { 
 
@@ -18,6 +19,8 @@ function requestPV() {
 			json.type = "PV";
 			var entity = NGSI.parseEntity(json);
 			console.log(entity);
+
+			postEntity(entity);
 		}
 	);
 
@@ -35,9 +38,31 @@ function requestWeather() {
 			json.type = "WeatherObserved";
 			var entity = NGSI.parseEntity(json);
 			console.log(entity);
+
+			postEntity(entity);
 		}
 	);
 } 
+
+function postEntity(entity) {
+	fetch(contextBrokerEntitiesURL, {
+		method : 'POST',
+		headers : {
+			'Accept' : 'application/json',
+			'Content-Type' : 'application/json'
+		},
+		body : JSON.stringify(entity)
+	})
+	.then(res => res.json())
+	.then(resJSON => {
+		console.log(resJSON);
+	})
+	.catch((err) => {
+		console.log(err);
+		reject(err);
+	});
+}
+
 
 setInterval(requestPV, 900000);  // function runs every 15 minutes
 setInterval(requestWeather, 60000);  // function runs every minute
